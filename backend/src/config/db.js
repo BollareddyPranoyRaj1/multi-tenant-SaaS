11f19da4ca0cc595b898a0c13e1115aacbc46b40
Bollareddy Pranoy Raj <bollareddypranoyraj@gmail.com>
2025-12-25 00:18:28 +0530

    Set default values for database connection config

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST || 'database',
+  port: process.env.DB_PORT || 5432,
+  user: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_DATABASE || 'multi_tenant_saas',
 });
 
 module.exports = pool;
